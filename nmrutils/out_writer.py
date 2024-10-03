75d2b0ab4d356811019f6c53ce0f661a9b15ce28
silvanasilag <114257117+silvanasilag@users.noreply.github.com>
2024-10-03 10:52:37 -0600

    ya arreglamos los pobemas del print, no sabemos que era pero lo arreglamos cambiando el open con un with.

--- nmrutils/out_writer.py
+++ nmrutils/out_writer.py
@@ -19,6 +19,7 @@ from nmrutils.regression import scale,stat,splot,xy
 from sklearn.metrics import mean_squared_error
 from nmrutils.isotropicreader import molecules_data
 from nmrutils.getbilparam import get_a_str, read_block_of_inp, get_a_int, get_a_float,key_norm
+from glomos_utils.atomic import Dr_TS
 
 def fechayhora():
     months=["Jan","Feb","March","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
@@ -142,126 +143,127 @@ def out_w(path,data,new,tbl_comp,keys):
     plt.clf()
     splot(xcc,ycc,plotc_name,data_c.m,data_c.b,data_c.r2,"Carbono-13","steelblue")
     if len(xn_h) != 0:splot(xn_c,yn_c,plotc_name,data_c.m,data_c.b,data_c.r2,"Carbono-13","salmon")
-    out =str(path)+"/scaled_hormons.txt"
-    out=open(out,'a')
-    out.write("CHEMical Shift Scaler\n\n")
-    out.write("This software is provided by TheoChem Merida. \n")
-    out.write("Code by Silvana Silva-Aguirre, Filiberto Ortiz-Chi and Gabriel Merino \n")
-    out.write("Username: "+os.path.split(os.path.expanduser('~'))[1]+"\n\n")
-    out.write("Date: "+fechayhora()+"\n\n")
-    if len(new) == 0:
-        out.write("Total Job cpu time: %s\n\n"%(str(cputime+cputime_nmr)))
-        out.write("Total Job cpu opt: %s\n\n"%(str(cputime)))
-        out.write("Total Job cpu nmr: %s\n\n\n"%(str(cputime_nmr)))
-    else:
-        out.write("Total Job cpu time: %s\n\n"%(str(cputime+cputime_nmr+cputime_n+cputime_nmr_n)))
-        out.write("CPU time for DataSet: \n")
-        out.write("--------------------------------\n")
-        out.write("Total Job cpu opt: %s\n"%(str(cputime)))
-        out.write("Total Job cpu nmr: %s\n\n"%(str(cputime_nmr)))
-        out.write("CPU time for TestSet: \n")
-        out.write("--------------------------------\n")
-        out.write("Total Job cpu opt: %s\n"%(str(cputime_n)))
-        out.write("Total Job cpu nmr: %s\n\n"%(str(cputime_nmr_n)))
-    out.write("------------------------------------------------------------------------------------------------\n")
-    out.write("The Geometry optimization and the isotropic shielding constants were computed using Gaussian G16\n")
-    out.write("------------------------------------------------------------------------------------------------\n")
-    out.write("{:<24}{:>47}\n".format("Geometry OPT","NMR"))                  #Datos a extraer del out
-    out.write("------------------------------------------------------------------------------------------------\n")
-    out.write("{:<47s}{:>24s}\n".format(str(key_opt),str(key_nmr)))
-    out.write("\n\n||*********************Data for 1H******************||\n\n")
-    out.write("Scaling Factors 1H           \n") #Datos a extraer del out
-    out.write("------------------\n") 
-    out.write('{0:5} {1:10} \n'.format("slope","     intercept"))
-    out.write("------------------\n")
-    out.write('{0:5.4f} {1:-10.4f}\n\n'.format(data_h.m, data_h.b))
-    out.write("Performance     1H                \n") #Datos a extraer del out
-    out.write("------------------\n")
-    out.write('{0:5} {1:10} \n'.format("RMSD", "     R^2"))
-    out.write("----------------- \n") 
-    out.write('{0:5.4f} {1:-10.4f} \n'.format(data_h.rmsd,data_h.r2))    
-    out.write("\n\n||*********************Data for 13C******************|| \n\n")
-    out.write("Scaling Factors 13C\n") #Datos a extraer del out
-    out.write("-----------------\n") 
-    out.write('{0:5} {1:10} \n'.format("slope","     intercept"))
-    out.write("------------------ \n")
-    out.write('{0:5.4f} {1:-10.4f} \n\n'.format(data_c.m, data_c.b))
-    out.write("Performance   13C\n") #Datos a extraer del out
-    out.write("-----------------\n")
-    out.write('{0:5} {1:10} \n'.format("RMSD", "     R^2"))
-    out.write("----------------- \n") 
-    out.write('{0:5.4f} {1:-10.4f} \n'.format(data_c.rmsd,data_c.r2))
-    out.write("\n\n|******************************************|\n")
-    out.write("|                Test Set Results          |\n") #Datos a extraer del out
-    out.write("|******************************************|\n")   
-    out.write("Name                                         Isotropic Values    Exp.Values    Shift Value(scaled)    Residual^2 \n")
-    if len(new)==0:
-        for imol in data:
-            out.write("------------------------------------\n")
-            out.write("%s\n"%(imol.im))
-            out.write("%s\n"%(imol.i))
-            ph,pc=[],[]
-            for iatom in imol.atoms:
-                symbol= iatom.s
-                if symbol == "H":
-                    out.write("%-5s %-30s" %(iatom.s,iatom.nz))
-                    out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
-                    ph.append((iatom.r))
-                if symbol == "C":            
-                    out.write("%-5s %-30s" %(iatom.s,iatom.nz))
-                    out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
-                    pc.append((iatom.r))
-            if len(ph)!= 0 :
-                rmsdh = (sum(ph)/len(ph))**0.5
-                out.write("rmsd of H: %25.4F \n"%(rmsdh))
-            if len(pc)!= 0 :   
-                rmsdc = (sum(pc)/len(pc))**0.5
-                out.write("rmsd of C: %25.4f \n"%(rmsdc))
-            if imol.chk==1: out.write("Chk: YES \n")
-        if tbl_comp != "-": comp_table(tbl_comp,data_c,data_h,keys,str(path.split('/')[-1]),0,0,cputime,cputime_nmr)
+    with open(str(path) + "/scaled_hormons.txt", 'a') as out:
+        out.write("CHEMical Shift Scaler\n\n")
+        out.write("This software is provided by TheoChem Merida. \n")
+        out.write("Code by Silvana Silva-Aguirre, Filiberto Ortiz-Chi and Gabriel Merino \n")
+        out.write("Username: "+os.path.split(os.path.expanduser('~'))[1]+"\n\n")
+        out.write("Date: "+fechayhora()+"\n\n")
+        if len(new) == 0:
+            out.write("Total Job cpu time: %s\n\n"%(str(cputime+cputime_nmr)))
+            out.write("Total Job cpu opt: %s\n\n"%(str(cputime)))
+            out.write("Total Job cpu nmr: %s\n\n\n"%(str(cputime_nmr)))
+        else:
+            out.write("Total Job cpu time: %s\n\n"%(str(cputime+cputime_nmr+cputime_n+cputime_nmr_n)))
+            out.write("CPU time for DataSet: \n")
+            out.write("--------------------------------\n")
+            out.write("Total Job cpu opt: %s\n"%(str(cputime)))
+            out.write("Total Job cpu nmr: %s\n\n"%(str(cputime_nmr)))
+            out.write("CPU time for TestSet: \n")
+            out.write("--------------------------------\n")
+            out.write("Total Job cpu opt: %s\n"%(str(cputime_n)))
+            out.write("Total Job cpu nmr: %s\n\n"%(str(cputime_nmr_n)))
+        out.write("------------------------------------------------------------------------------------------------\n")
+        out.write("The Geometry optimization and the isotropic shielding constants were computed using Gaussian G16\n")
+        out.write("------------------------------------------------------------------------------------------------\n")
+        out.write("{:<24}{:>47}\n".format("Geometry OPT","NMR"))                  #Datos a extraer del out
+        out.write("------------------------------------------------------------------------------------------------\n")
+        out.write("{:<47s}{:>24s}\n".format(str(key_opt),str(key_nmr)))
+        out.write("\n\n||*********************Data for 1H******************||\n\n")
+        out.write("Scaling Factors 1H           \n") #Datos a extraer del out
+        out.write("------------------\n")
+        out.write('{0:5} {1:10} \n'.format("slope","     intercept"))
+        out.write("------------------\n")
+        out.write('{0:5.4f} {1:-10.4f}\n\n'.format(data_h.m, data_h.b))
+        out.write("Performance     1H                \n") #Datos a extraer del out
+        out.write("------------------\n")
+        out.write('{0:5} {1:10} \n'.format("RMSD", "     R^2"))
+        out.write("----------------- \n")
+        out.write('{0:5.4f} {1:-10.4f} \n'.format(data_h.rmsd,data_h.r2))
+        out.write("\n\n||*********************Data for 13C******************|| \n\n")
+        out.write("Scaling Factors 13C\n") #Datos a extraer del out
+        out.write("-----------------\n")
+        out.write('{0:5} {1:10} \n'.format("slope","     intercept"))
+        out.write("------------------ \n")
+        out.write('{0:5.4f} {1:-10.4f} \n\n'.format(data_c.m, data_c.b))
+        out.write("Performance   13C\n") #Datos a extraer del out
+        out.write("-----------------\n")
+        out.write('{0:5} {1:10} \n'.format("RMSD", "     R^2"))
+        out.write("----------------- \n")
+        out.write('{0:5.4f} {1:-10.4f} \n'.format(data_c.rmsd,data_c.r2))
+        out.write("\n\n|******************************************|\n")
+        out.write("|                Test Set Results          |\n") #Datos a extraer del out
+        out.write("|******************************************|\n")
+        out.write("Name                                         Isotropic Values    Exp.Values    Shift Value(scaled)    Residual^2 \n")
+        if len(new)==0:
+            for imol in data:
+                out.write("------------------------------------\n")
+                out.write("%s\n"%(imol.im))
+                out.write("%s\n"%(imol.i))
+                ph,pc=[],[]
+                for iatom in imol.atoms:
+                    symbol= iatom.s
+                    if symbol == "H":
+                        out.write("%-5s %-30s" %(iatom.s,iatom.nz))
+                        out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
+                        ph.append((iatom.r))
+                    if symbol == "C":
+                        out.write("%-5s %-30s" %(iatom.s,iatom.nz))
+                        out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
+                        pc.append((iatom.r))
+                if len(ph)!= 0 :
+                    rmsdh = (sum(ph)/len(ph))**0.5
+                    out.write("rmsd of H: %25.4F \n"%(rmsdh))
+                if len(pc)!= 0 :
+                    rmsdc = (sum(pc)/len(pc))**0.5
+                    out.write("rmsd of C: %25.4f \n"%(rmsdc))
+                if imol.chk==1: out.write("Chk: YES \n")
+            if tbl_comp != "-": comp_table(tbl_comp,data_c,data_h,keys,str(path.split('/')[-1]),0,0,cputime,cputime_nmr)
 
-    else:
-        pph,ppc=[],[]
-        rmsdh,rmsdc=0,0
-        for imol in new:
-            out.write("------------------------------------\n")
-            out.write("%s\n"%(imol.im))
-            out.write("%s\n"%(imol.i))
-            ph,pc=[],[]
-            for iatom in imol.atoms:
-                symbol= iatom.s
-                if symbol == "H":
-                    out.write("%-5s %-30s" %(iatom.s,iatom.nz))
-                    out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
-                    ph.append((iatom.r))
-                    pph.append((iatom.r))
-                if symbol == "C":            
-                    out.write("%-5s %-30s" %(iatom.s,iatom.nz))
-                    out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
-                    pc.append((iatom.r))
-                    ppc.append((iatom.r))
-            if len(ph)!= 0 :
-                rmsdh = (sum(ph)/len(ph))**0.5
-                out.write("rmsd of H: %25.4F \n"%(rmsdh))
-            if len(pc)!= 0 :
-                rmsdc = (sum(pc)/len(pc))**0.5
-                out.write("rmsd of C: %25.4f \n"%(rmsdc))
-        if len(pph)!= 0 :
-            rmsdh = (sum(pph)/len(pph))**0.5
-            rmsdh = round(rmsdh,4)
-            out.write("\n\ngeneral rmsd of H: %25.4F \n"%(rmsdh))
-            print ("\n----Test error 1H -----")
-            print("general rmsd of 1H:",rmsdh)
-        if len(ppc)!= 0 :   
-            rmsdc = (sum(ppc)/len(ppc))**0.5
-            rmsdc = round(rmsdc,4)
-            out.write("general rmsd of C: %25.4f \n"%(rmsdc))
-            print ("\n----Test error 13C -----")
-            print("general rmsd of 13C:",rmsdc)
-        if imol.chk==1: out.write("Chk: YES \n")
-        if tbl_comp != "-": comp_table(tbl_comp,data_c,data_h,keys,str(path.split('/')[-1]),rmsdh,rmsdc,cputime_n,cputime_nmr_n)
-    out.write("\n\nThat might sound boring, but I think the boring stuff is the stuff I remember the most\n\n")
-    out.write("**** Don't get amxiaty, there was no problem whatsoever ****\n")
-    out.close
+        else:
+            pph,ppc=[],[]
+            rmsdh,rmsdc=0,0
+            for imol in new:
+                out.write("------------------------------------\n")
+                out.write("%s\n"%(imol.im))
+                out.write("%s\n"%(imol.i))
+                ph,pc=[],[]
+                for iatom in imol.atoms:
+                    symbol= iatom.s
+                    if symbol == "H":
+                        out.write("%-5s %-30s" %(iatom.s,iatom.nz))
+                        out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
+                        ph.append((iatom.r))
+                        pph.append((iatom.r))
+                    if symbol == "C":
+                        out.write("%-5s %-30s" %(iatom.s,iatom.nz))
+                        out.write('{0:-18.4} {1:16.2f} {2:-17.2f} {3:-18.4f} \n'.format(iatom.t, iatom.e, iatom.c, iatom.r))
+                        pc.append((iatom.r))
+                        ppc.append((iatom.r))
+                if len(ph)!= 0 :
+                    rmsdh = (sum(ph)/len(ph))**0.5
+                    out.write("rmsd of H: %25.4F \n"%(rmsdh))
+                if len(pc)!= 0 :
+                    rmsdc = (sum(pc)/len(pc))**0.5
+                    out.write("rmsd of C: %25.4f \n"%(rmsdc))
+            if len(pph)!= 0 :
+                rmsdh = (sum(pph)/len(pph))**0.5
+                rmsdh = round(rmsdh,4)
+                out.write("\n\ngeneral rmsd of H: %25.4F \n"%(rmsdh))
+                print ("\n----Test error 1H -----")
+                print("general rmsd of 1H:",rmsdh)
+            if len(ppc)!= 0 :
+                rmsdc = (sum(ppc)/len(ppc))**0.5
+                rmsdc = round(rmsdc,4)
+                out.write("general rmsd of C: %25.4f \n"%(rmsdc))
+                print ("\n----Test error 13C -----")
+                print("general rmsd of 13C:",rmsdc)
+            if imol.chk==1: out.write("Chk: YES \n")
+            if tbl_comp != "-": comp_table(tbl_comp,data_c,data_h,keys,str(path.split('/')[-1]),rmsdh,rmsdc,cputime_n,cputime_nmr_n)
+
+#        out.write("\n\nThat might sound boring, but I think the boring stuff is the stuff I remember the most\n\n")
+        out.write("\n\n" + Dr_TS() + "\n\n")
+        out.write("**** Don't get amxiaty, there was no problem whatsoever ")
+
 
     return keys[0]
